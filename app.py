6264bcb0c6d5f0f48184444cae31a64c4af0d9e3
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-01-24 22:23:10 -0300

    Adding User and Meal models

--- app.py
+++ app.py
@@ -2,5 +2,34 @@ import bcrypt
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models.user import User
+from models.meal import Meal
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin123@localhost:3306/daily-diet'
+
+login_manager = LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Create User
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+
+  if username and password: 
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password, role='user')
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User successfully created.'})
+
+  return jsonify({'message': 'Invalid data'}), 400
+
+if __name__ == '__main__':
+  app.run(debug=True)
